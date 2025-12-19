deb447c4a0ebad3a9359f76ae435e9e016850ecd
Slastenin Nikolay <slastenindev@gmail.com>
2025-12-19 15:28:56 +0300

    Fixed: file post uploads

--- cms/src/api/otkliki-na-vakansii/routes/otkliki-na-vakansii.ts
+++ cms/src/api/otkliki-na-vakansii/routes/otkliki-na-vakansii.ts
@@ -4,31 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-const defaultRouter = factories.createCoreRouter('api::otkliki-na-vakansii.otkliki-na-vakansii');
-
-const customRouter = (innerRouter, extraRoutes = []) => {
-  let routes;
-  return {
-    get prefix() {
-      return innerRouter.prefix;
-    },
-    get routes() {
-      if (!routes) routes = [...extraRoutes, ...innerRouter.routes];
-      return routes;
-    },
-  };
-};
-
-const myExtraRoutes = [
-  {
-    method: 'POST',
-    path: '/otkliki-na-vakansiis',
-    handler: 'otkliki-na-vakansii.create',
-    config: {
-      policies: [],
-      middlewares: [],
-    },
-  },
-];
-
-export default customRouter(defaultRouter, myExtraRoutes);
+export default factories.createCoreRouter('api::otkliki-na-vakansii.otkliki-na-vakansii');
