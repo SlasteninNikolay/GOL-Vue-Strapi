51ccaf04fc748eef225ba39d68c96116feca361f
Slastenin Nikolay <slastenindev@gmail.com>
2025-12-19 10:59:49 +0300

    Fixed: cms cors policy

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -12,8 +12,9 @@ export default [
         name: 'strapi::cors',
         config: {
             origin: ['http://localhost:3000', 'http://localhost:5173', "https://legenda-hotels.ru", "https://cms.legenda-hotels.ru"],
-            methods: ['GET', 'POST', 'OPTIONS'],
-            headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+            methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+            headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'Access-Control-Allow-Origin'],
+            credentials: true,
             keepHeaderOnError: true,
         },
     },
