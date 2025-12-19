f57c9c25bf79f2fac3cb6af8cf8862b5931f2fe2
Slastenin Nikolay <slastenindev@gmail.com>
2025-12-19 15:51:38 +0300

    Fixed: file post uploads

--- cms/src/api/otkliki-na-vakansii/routes/otkliki-na-vakansii.ts
+++ cms/src/api/otkliki-na-vakansii/routes/otkliki-na-vakansii.ts
@@ -2,6 +2,17 @@
  * otkliki-na-vakansii router
  */
 
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::otkliki-na-vakansii.otkliki-na-vakansii');
+export default {
+  routes: [
+    {
+      method: 'POST',
+      path: '/otkliki-na-vakansiis',
+      handler: 'otkliki-na-vakansii.create',
+      config: {
+        auth: false, // Отключаем аутентификацию для публичного доступа
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
